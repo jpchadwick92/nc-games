e2310d166626f96d18affdf8c51fc05ea36cd628
jpchadwick92 <jpchadwick92@gmail.com>
2022-09-28 14:51:49 +0100

    add category pages

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,13 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { fetchCategories } from "../utils/api";
 const Nav = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    axios
-      .get("https://northcoders-games-project.herokuapp.com/api/categories")
-      .then(({ data }) => {
-        setCategories(data.categories);
-      });
+    fetchCategories().then(({ categories }) => {
+      setCategories(categories);
+    });
   }, []);
   return (
     <nav className="nav">
--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -1,21 +1,20 @@
 import { useEffect, useState } from "react";
 import ReviewCard from "./ReviewCard";
 import Loading from "./Loading";
-import axios from "axios";
+import { useParams } from "react-router-dom";
+import { fetchReviews } from "../utils/api";
 
 const ReviewList = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [reviews, setReviews] = useState([]);
-
+  const { category } = useParams();
   useEffect(() => {
     setIsLoading(true);
-    axios
-      .get("https://northcoders-games-project.herokuapp.com/api/reviews")
-      .then(({ data }) => {
-        setReviews(data.reviews);
-        setIsLoading(false);
-      });
-  }, []);
+    fetchReviews(category).then(({ reviews }) => {
+      setReviews(reviews);
+      setIsLoading(false);
+    });
+  }, [category]);
 
   return (
     <section>
